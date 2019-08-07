a61ae975d9ad424032408317354892aa9d599097
dc pc <ramana.sakhavarapu@gmail.com>
2019-08-07 18:08:22 +0530

    working navigation and icon Aug 7 evng 6

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+    
 /**
  * Sample React Native App
  * https://github.com/facebook/react-native
@@ -6,7 +7,7 @@
  * @flow
  */
 
-import React, {Fragment} from 'react';
+import React, { Fragment } from 'react';
 import {
   SafeAreaView,
   StyleSheet,
@@ -14,6 +15,7 @@ import {
   View,
   Text,
   StatusBar,
+  Button
 } from 'react-native';
 
 import {
@@ -23,59 +25,206 @@ import {
   DebugInstructions,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
+
 import Icon from 'react-native-vector-icons/EvilIcons';
+import { createStackNavigator, createAppContainer,createDrawerNavigator,router } from "react-navigation";
 
-const App = () => {
-  return (
-    <Fragment>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>
-              <Icon  name='trophy' size={30}/>Step RAM2</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-          <View>
+export default class App extends React.Component {
+  render() {
+    return <AppContainer />;
+  }
+}
 
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </Fragment>
-  );
-};
+
+class HomeScreen extends React.Component {
+  // static navigationOptions={
+  //   headerStyle:{
+  //     backgroundColor:'red'
+  //   }
+  // };
+  static navigationOptions = {
+    title: 'Home',
+  };
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Text>Home Screen h</Text>
+        <Text><Icon  name='trophy' size={30}/>icon</Text>
+        <Button title="Go to Details"
+        onPress={()=> this.props.navigation.navigate('Details')}/>
+      </View>
+    );
+  }
+}
+class DashboardScreen extends React.Component {
+   static navigationOptions={
+    headerStyle:{
+      backgroundColor:'red'
+    }
+  };
+  static navigationOptions = {
+    title: 'Dashboard',
+  };
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center",borderWidth:15,borderColor:'green' }}>
+        <Text>Dashboard</Text>
+        <Button title="Go to Details"
+        onPress={()=> this.props.navigation.navigate('Details')}/>
+      </View>
+    );
+  }
+}
+class DetailsScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Details',
+  };
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Text>Details Screen</Text>
+        <Button
+          title="Go to Details... again"
+          onPress={() => this.props.navigation.navigate('Details')}
+        />
+        <Button
+          title="Go to Home... again"
+          onPress={() => this.props.navigation.navigate('Home')}
+        />
+      </View>
+    );
+  }
+}
+
+
+const AppNavigator = createStackNavigator({
+    Home : HomeScreen,
+    Details:DetailsScreen
+},
+{
+  initialRouteName:"Home",
+    defaultNavigationOptions: {
+      headerStyle:{
+        backgroundColor:'orange'
+      },
+      headerTintColor:'red',
+      headerTitleStyle:{
+        fontWeight:'bold',
+      },
+    }  
+}
+);
+
+
+
+const AppDrawerNavigator = createDrawerNavigator(
+  {
+    Home : HomeScreen,
+    Dashboard:DashboardScreen,
+    Details:DetailsScreen,
+},
+{
+  initialRouteName:"Home",
+    defaultNavigationOptions: {
+      headerStyle:{
+        backgroundColor:'blue'
+      },
+      headerTintColor:'pink',
+      headerTitleStyle:{
+        fontWeight:'bold',
+      },
+    } 
+}
+);
+
+//const AppContainer = createAppContainer(AppDrawerNavigator);
+const AppContainer = createAppContainer(AppNavigator);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const App = () => {
+//      return (
+//        <View>
+//          <Text>SFSDFDSF</Text>
+//        </View>
+//      );
+// };
+
+
+
+// const App = () => {
+//   return (
+//     <Fragment>
+//       <StatusBar barStyle="dark-content" />
+//       <SafeAreaView>
+//         <ScrollView
+//           contentInsetAdjustmentBehavior="automatic"
+//           style={styles.scrollView}>
+//           <Header />
+//           {global.HermesInternal == null ? null : (
+//             <View style={styles.engine}>
+//               <Text style={styles.footer}>Engine: Hermes</Text>
+//             </View>
+//           )}
+//           <View style={styles.body}>
+//             <View style={styles.sectionContainer}>
+//               <Text style={styles.sectionTitle}>Step One</Text>
+//               <Text style={styles.sectionDescription}>
+//                 Edit <Text style={styles.highlight}>App.js</Text> to change this
+//                 screen and then come back to see your edits.
+//               </Text>
+//             </View>
+//             <View style={styles.sectionContainer}>
+//               <Text style={styles.sectionTitle}>See Your Changes</Text>
+//               <Text style={styles.sectionDescription}>
+//                 <ReloadInstructions />
+//               </Text>
+//             </View>
+//             <View style={styles.sectionContainer}>
+//               <Text style={styles.sectionTitle}>Debug</Text>
+//               <Text style={styles.sectionDescription}>
+//                 <DebugInstructions />
+//               </Text>
+//             </View>
+//             <View style={styles.sectionContainer}>
+//               <Text style={styles.sectionTitle}>Learn More</Text>
+//               <Text style={styles.sectionDescription}>
+//                 Read the docs to discover what to do next:
+//               </Text>
+//             </View>
+//             <LearnMoreLinks />
+//           </View>
+//         </ScrollView>
+//       </SafeAreaView>
+//     </Fragment>
+//   );
+// };
 
 const styles = StyleSheet.create({
   scrollView: {
@@ -116,4 +265,5 @@ const styles = StyleSheet.create({
   },
 });
 
-export default App;
+// export default App;
+
