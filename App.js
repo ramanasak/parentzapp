ca293498495cad16c8f2f3437480367ebbe360c6
dc pc <ramana.sakhavarapu@gmail.com>
2019-08-09 14:07:59 +0530

    applied icons and adjustments to notice and menu screen

--- App.js
+++ App.js
@@ -33,8 +33,8 @@ import {
 //import Icon from 'react-native-vector-icons/EvilIcons';
 import Icon from 'react-native-vector-icons/Feather';
 import { createStackNavigator, createAppContainer, createDrawerNavigator, router } from "react-navigation";
-
-export default class menuscreen extends React.Component {
+//default
+export class menuscreen extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
@@ -53,6 +53,7 @@ export default class menuscreen extends React.Component {
     const buttonBg = button_toggle ? "#3700b3" : "#ff00ff"; //dark purple and magenta
     return (
       <View style={styles.container}>
+        <StatusBar backgroundColor="white" barStyle="dark-content" />
         <View style={{ flexDirection: 'row', borderWidth: 0 }}>
           <View style={{ flex: 1 }}>
             <Image style={styles.imageView2} source={require('./1.jpg')} />
@@ -133,13 +134,212 @@ export default class menuscreen extends React.Component {
           </TouchableOpacity>
         </View>
 
-      </View >
+      </View>
     );
   }
 
-}
+}//menuscreen
+
+export default class Notice extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { isLoading: true }
+  }//constructor
 
 
+  //https://facebook.github.io/react-native/movies.json
+  //https://jsonplaceholder.typicode.com/posts
+
+  renderSeparator = () => {
+    return (
+      <View style={{ height: 1, width: '100%', backgroundColor: 'grey' }}>
+      </View>
+    )
+  }
+  renderItem = ({ item }) => {
+    return (
+      <TouchableOpacity style={styles.list} onPress={() => ToastAndroid.show(item.title, ToastAndroid.SHORT)}>
+        <View style={{ flex: 1, flexDirection: 'row', backgroundColor: 'white', justifyContent: 'center' }}>
+          <View style={{
+            flex: 2, justifyContent: 'center', marginLeft: 15, backgroundColor: 'white',
+            borderColor: 'black', borderWidth: 1, borderRadius: 10, borderColor: 'white'
+          }}>
+            <Text style={{ fontSize: 18, color: '#3f51b5', marginBottom: 1, marginTop: 1, justifyContent: 'center', marginLeft: 20 }}>
+              {item.title}, {item.releaseYear}
+            </Text>
+            <Text style={{ fontSize: 14, color: '#3f51b5', marginBottom: 5, marginLeft: 20 }}>
+              {item.releaseYear}
+            </Text>
+          </View>
+          <View style={{ flex: 1, justifyContent: 'center', backgroundColor: 'white' }}>
+            <Text style={{ fontSize: 18, color: '#3f51b5', marginBottom: 1, marginTop: 1, justifyContent: 'center' }}>
+              <Icon name="paperclip" size={20} />
+            </Text>
+          </View>
+        </View>
+      </TouchableOpacity>
+
+    )
+  }
+
+  componentDidMount() {
+
+    return fetch('https://facebook.github.io/react-native/movies.json')
+      .then((response) => response.json())
+      .then((responseJson) => {
+        console.log(responseJson);
+        this.setState({
+          dataSource: responseJson.movies,
+          isLoading: false
+        }, function () { }
+        );
+
+      }).catch((error) => {
+        console.log(error)
+      })
+
+  }//asdasdad
+
+
+
+  // componentDidMount() {
+  //   return fetch('https://facebook.github.io/react-native/movies.json')
+  //     .then((response) => response.json())
+  //     .then((responseJson) => {
+
+  //       this.setState({
+  //         isLoading: false,
+  //         dataSource: responseJson.movies,
+  //       }, function () {
+
+  //       });
+
+  //     })
+  //     .catch((error) => {
+  //       console.error(error);
+  //     });
+  // }
+
+  // render() {
+  //   // if (this.state.isLoading == true) {
+  //   //   return (
+  //   //     <View Style={StyleSheet.container}>
+  //   //       <ActivityIndicator size="Large" animating />
+  //   //     </View>
+  //   //   )
+  //   // } else {
+  //   return (
+  //     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }} >
+  //       <Text>Notice Screen</Text>
+  //       {/* <Button title="Go to Details"
+  //                 onPress={() => this.props.navigation.navigate('Details')} /> 
+  //                  //data={this.state.dataSource}
+  //                  */}
+
+  //       {/* <FlatList
+  //           data={[{ key: 'a' }, { key: 'b' }]}
+  //           renderItem={({ item }) => <Text>{item.key}</Text>}
+  //         /> */}
+  //       <FlatList
+  //         data={this.state.dataSource}
+  //         renderItem={this.state.renderItem}
+  //       />
+  //     </View>
+  //   );
+  //   //}else
+
+  // }
+
+  render() {
+
+    if (this.state.isLoading) {
+      return (
+        <View style={{ flex: 1, padding: 20 }}>
+          <ActivityIndicator />
+        </View>
+      )
+    }
+    //style={{ backgroundColor: 'white' }}
+    return (
+      <ScrollView style={{ color: "white", fontSize: 18, textAlign: 'center' }}>
+        <StatusBar backgroundColor="#fff" barStyle="dark-content" />
+
+        <View style={{ flexDirection: 'row', borderWidth: 0, backgroundColor: 'white', marginTop: 10 }}>
+          <View style={{ flex: 1 }}>
+            <Image style={styles.imageView2} source={require('./1.jpg')} />
+          </View>
+          <View style={{ flex: 3, backgroundColor: 'white' }}>
+
+            <View style={{ flex: 1, alignItems: 'stretch', justifyContent: 'space-around', backgroundColor: 'white' }}>
+              <View style={{ justifyContent: 'space-around', alignItems: 'flex-start', marginLeft: 35 }}>
+                <Text style={{ borderWidth: 0, justifyContent: 'space-around', fontSize: 18, alignItems: 'center' }}>
+                  <Icon name="user" size={15} /> Ramana Sakhavarapu</Text>
+                {/* <Text style={{ fontSize: 10, color: 'grey' }}>NAme</Text> */}
+                {/* <Icon name="award" size={30} /> */}
+              </View>
+            </View>
+
+
+
+            <View style={{ flexDirection: 'row', justifyContent: 'space-around' }}>
+              <View style={{ alignItems: 'center' }}>
+                <Text>6</Text>
+                <Text style={{ fontSize: 10, color: 'grey' }}>Class</Text>
+              </View>
+              <View style={{ alignItems: 'center' }}>
+                <Text>A</Text>
+                <Text style={{ fontSize: 10, color: 'grey' }}>Section</Text>
+              </View>
+              <View style={{ alignItems: 'center' }}>
+                <Text>112</Text>
+                <Text style={{ fontSize: 10, color: 'grey' }}>Roll</Text>
+              </View>
+            </View>
+
+          </View>
+        </View>
+
+
+
+
+
+        <View style={{ marginTop: 10 }}>
+          <Text style={{ color: "#303f9f", fontSize: 20, textAlign: 'center', marginBottom: 15, marginTop: 10 }}>
+            <Icon name="file-text" size={20} />  NOTICES</Text>
+        </View>
+        <View>
+          <FlatList
+            //ItemSeparatorComponent={this.renderSeparator}
+            data={this.state.dataSource}
+            renderItem={this.renderItem}
+            keyExtractor={({ id }, index) => id}
+            ItemSeparatorComponent={this.renderSeparator}
+          />
+        </View>
+        {/* <View style={{ backgroundColor: 'blue', flexDirection: 'row', flex: 1 }}>
+          <View style={{ backgroundColor: 'red', flexDirection: 'row', flex: 1 }}>
+            <View style={{ backgroundColor: 'orange', flexDirection: 'column', flex: 1 }}>
+              <Text style={{ color: "#303f9f", fontSize: 20, textAlign: 'center', marginBottom: 15, marginTop: 10 }}>
+                <Icon name="file-text" size={20} />  test</Text>
+              <Text style={{ color: "#303f9f", fontSize: 20, textAlign: 'center', marginBottom: 15, marginTop: 10 }}>
+                <Icon name="file-text" size={20} />  test</Text>
+            </View>
+
+          </View>
+          <View style={{ backgroundColor: 'yellow', flexDirection: 'row', flex: 1 }}>
+            <Text style={{ color: "#303f9f", fontSize: 20, textAlign: 'center', marginBottom: 15, marginTop: 10 }}>
+              <Icon name="file-text" size={20} />  test</Text>
+          </View>
+
+
+        </View> */}
+
+      </ScrollView>
+    );
+  }
+
+}//notice 
 
 
 
@@ -488,14 +688,14 @@ const styles = StyleSheet.create({
     marginVertical: 10,
     borderRadius: 5,
     borderWidth: 1,
-    borderColor: "#303f9f"
+    borderColor: "#2b388f"
   },
   list: {
     backgroundColor: "white",
     width: 500,
-    paddingVertical: 8,
-    marginVertical: 10,
-    borderRadius: 5
+    paddingVertical: 0,
+    marginVertical: 0,
+    borderRadius: 10
   },
   imageView: {
     width: 70,
